af9b14d985d0d6ed5a03b8676bde23b1826b0f6f
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-11 23:58:02 +0300

    Add project files.

---
+++ problem6/Program.cs
@@ -0,0 +1,40 @@
+﻿  public class Student
+{
+    public Student(string v1, int v2)
+    {
+        V1 = v1;
+        V2 = v2;
+    }
+
+    public string name { get; set; }
+    public int age { get; set; }
+    public string V1 { get; }
+    public int V2 { get; }
+
+    public void student(string Name, int Age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public void get()
+    {
+        Console.WriteLine("name : {0} ", name);
+        Console.WriteLine("age : {0} ", age);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student1 = new("mohamed", 20);
+        student1.get();
+        Console.ReadLine();
+
+    }
+    
+}
+
+
+
+
+
